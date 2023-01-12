79e57b130255be7c9d41653b668b9af1c2ddabaa
John Christopher <jgchristopher@gmail.com>
2023-01-12 12:04:04 -0600

    Showing list of our boards

--- lib/retro_board_web/live/board_live.ex
+++ lib/retro_board_web/live/board_live.ex
@@ -1,3 +1,13 @@
 defmodule RetroBoardWeb.BoardLive do
   use RetroBoardWeb, :live_view
+
+  alias RetroBoard.Boards
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:boards, Boards.list_boards())
+     |> assign(:page_title, "Our Boards")}
+  end
 end
